dc17230c12f74d3f90f0bc4db7a9b0e84107d343
uno-nueve <118061603+uno-nueve@users.noreply.github.com>
2022-12-12 14:38:33 -0300

    Add files via upload

---
+++ Ejercicio-2/2.js
@@ -0,0 +1,14 @@
+const lista = [
+    "Luciano Montilla",
+    26,
+    true,
+    new Date("november 6 1995"),
+    libro = {
+        titulo: "La vuelta al mundo en 80 días",
+        autor: "Julio Verne",
+        fecha: 1872,
+        link: new URL("https://biblioteca.org.ar/libros/656605.pdf")
+    }
+]
+
+console.log(lista)
