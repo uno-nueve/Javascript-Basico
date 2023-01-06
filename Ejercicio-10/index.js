e985e06239c4d11a34f4526488b288962ce24c69
uno-nueve <118061603+uno-nueve@users.noreply.github.com>
2023-01-06 16:09:16 -0300

    Add files via upload

---
+++ Ejercicio-10/index.js
@@ -0,0 +1,7 @@
+import * as math from './modulos/controller.js';
+import chalk from 'chalk';
+
+const multi = math.multiplica(1, 2);
+console.log(multi);
+const sum = math.suma(4, 5);
+console.log(chalk.green(sum));
