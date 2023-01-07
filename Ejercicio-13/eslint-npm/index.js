471f9d1cec5764319485934b2777c5954cfffd23
uno-nueve <118061603+uno-nueve@users.noreply.github.com>
2023-01-07 17:01:20 -0300

    Add files via upload

---
+++ Ejercicio-13/eslint-npm/index.js
@@ -0,0 +1,13 @@
+// Strings base
+let nombre = "Luciano"
+let apellido = "Montilla"
+
+// Manipulación de strings
+let estudiante = nombre + " " + apellido
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let num = estudiante.length
+let firstNombre = nombre.charAt(0)
+let lastApellido = apellido.charAt(7)
+let sinEspacio = `${nombre}${apellido}`
+let estaEn = estudiante.includes(nombre)
