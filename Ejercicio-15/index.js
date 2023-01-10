629295dd4b5b9b1189aa4b14ea47778859a3aa9d
uno-nueve <118061603+uno-nueve@users.noreply.github.com>
2023-01-10 18:24:12 -0300

    Add files via upload

---
+++ Ejercicio-15/index.js
@@ -0,0 +1,13 @@
+let nombre = 'Luciano'
+
+let apellido = 'Montilla'
+
+const nombreCompleto = { nombre, apellido }
+
+// sessionStorage.setItem('nombreCompleto', JSON.stringify(nombreCompleto))
+
+// localStorage.setItem('nombreCompleto', JSON.stringify(nombreCompleto))
+
+// document.cookie = `nuevaCookie=${nombreCompleto};max-age=${120}` 
+
+// console.log(document.cookie)
